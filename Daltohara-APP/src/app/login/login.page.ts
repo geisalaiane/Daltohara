4bc2a43846f110b5c690bab50c7b4866363a0df8
fabricioselotto <fabricioselottohard01@gmail.com>
2020-11-28 22:58:20 -0300

    attImagem

--- Daltohara-APP/src/app/login/login.page.ts
+++ Daltohara-APP/src/app/login/login.page.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LoadingController, ToastController } from "@ionic/angular";
+import { User } from "../models/user.mode";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
+  user = {} as User;
 
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async login(user: User) {
+    if (this.formValidation()) {
+      //show loader
+      let loader = this.loadingCtrl.create({
+        message: "Espere um pouco...",
+      });
+      (await loader).present();
+    }
   }
 
+  formValidation() {
+    if (!this.user.email) {
+      this.showToast("Entre com email");
+      return false;
+    }
+
+    if (!this.user.password) {
+      this.showToast("Entre com senha");
+      return false;
+    }
+
+    return true;
+  }
+
+  showToast(message: string) {
+    this.toastCtrl
+      .create({
+        message: message,
+        duration: 3000,
+      })
+      .then((toastData) => toastData.present());
+  }
 }
