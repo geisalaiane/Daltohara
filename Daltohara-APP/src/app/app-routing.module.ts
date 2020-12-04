d1f8c5f7ab26d342012a0fe817b5a7a195dafd74
fabricioselotto <fabricioselottohard01@gmail.com>
2020-12-04 00:35:16 -0300

    ScrollPage

--- Daltohara-APP/src/app/app-routing.module.ts
+++ Daltohara-APP/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'resultado',
     pathMatch: 'full'
   },
   {
--- Daltohara-APP/src/app/resultado/resultado.page.ts
+++ Daltohara-APP/src/app/resultado/resultado.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ViewChild } from "@angular/core";
+import { IonContent } from "@ionic/angular";
 
 @Component({
-  selector: 'app-resultado',
-  templateUrl: './resultado.page.html',
-  styleUrls: ['./resultado.page.scss'],
+  selector: "app-resultado",
+  templateUrl: "./resultado.page.html",
+  styleUrls: ["./resultado.page.scss"],
 })
 export class ResultadoPage implements OnInit {
+  @ViewChild(IonContent, { static: false }) content: IonContent;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  logScrollStart(event) {
+    console.log("logScrollStart : When Scroll Starts", event);
   }
 
+  logScrolling(event) {
+    console.log("logScrolling : When Scrolling", event);
+  }
+
+  logScrollEnd(event) {
+    console.log("logScrollEnd : When Scroll Ends", event);
+  }
+
+  ScrollToBottom() {
+    this.content.scrollToBottom(1500);
+  }
+
+  ScrollToTop() {
+    this.content.scrollToTop(1500);
+  }
+
+  ScrollToPoint(X, Y) {
+    this.content.scrollToPoint(X, Y, 1500);
+  }
 }
